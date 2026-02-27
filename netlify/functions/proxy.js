79db7ed708260896f7bc0d1e0d76e48d8c50b677
kuyatiri <115559330+kuyatiri@users.noreply.github.com>
2026-02-28 00:28:26 +0800

    Enhance proxy handler to include query string
    Updated the proxy handler to include the query string in the target URL and improved comments.

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,14 +1,15 @@
 exports.handler = async (event) => {
-  // Remove leading "/proxy/"
+  // Remove "/proxy/"
   const rawPath = event.path.replace(/^\/proxy\//, "");
-
   const decodedPath = decodeURIComponent(rawPath);
 
-  const targetUrl = "http://" + decodedPath;
+  // Get query string
+  const queryString = event.rawQuery ? "?" + event.rawQuery : "";
+
+  const targetUrl = "http://" + decodedPath + queryString;
 
   try {
     const response = await fetch(targetUrl);
-
     const buffer = await response.arrayBuffer();
 
     return {
