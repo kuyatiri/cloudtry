bf9df7dab29d7c3c32346e9f0184e1f624ce4c67
kuyatiri <115559330+kuyatiri@users.noreply.github.com>
2026-02-28 00:20:55 +0800

    Modify proxy handler to return target URL
    Updated proxy function to return target URL instead of fetching response.

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,21 +1,11 @@
 exports.handler = async (event) => {
-  const path = event.path.replace("/proxy/", "");
-  const targetUrl = "http://" + path;
+  const rawPath = event.path.replace("/.netlify/functions/proxy/", "");
+  const decodedPath = decodeURIComponent(rawPath);
+  const targetUrl = "http://" + decodedPath;
 
-  try {
-    const response = await fetch(targetUrl);
-    const buffer = await response.arrayBuffer();
-
-    return {
-      statusCode: response.status,
-      headers: {
-        "Content-Type": response.headers.get("content-type") || "application/octet-stream",
-        "Access-Control-Allow-Origin": "*"
-      },
-      body: Buffer.from(buffer).toString("base64"),
-      isBase64Encoded: true
-    };
-  } catch (err) {
-    return { statusCode: 500, body: "Proxy error" };
-  }
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "text/plain" },
+    body: targetUrl
+  };
 };
